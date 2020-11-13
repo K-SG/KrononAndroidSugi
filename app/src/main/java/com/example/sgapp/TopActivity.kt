e5dacf9c0efddfb73d092320a0bd033d8d941cf3
K_Sugi <67626325+SugiKoki@users.noreply.github.com>
2020-11-13 15:57:08 +0900

    名前を変更

--- app/src/main/java/com/example/sgapp/TopActivity.kt
+++ app/src/main/java/com/example/sgapp/TopActivity.kt
@@ -7,10 +7,10 @@ import android.widget.Button
 import android.widget.ImageView
 //import com.example.sgapp.ui.login.LoginActivity
 
-class MainActivity : AppCompatActivity() {
+class TopActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_top)
         var start_button = findViewById<ImageView>(R.id.start_button)
         start_button.setOnClickListener{
 //            val intent = Intent(this,LoginActivity::class.java)
