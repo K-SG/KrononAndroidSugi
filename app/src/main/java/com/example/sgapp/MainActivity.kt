8b88078ba32b9a3c518c2c734a923fa0ad57b9df
K_Sugi <67626325+SugiKoki@users.noreply.github.com>
2020-11-13 15:48:43 +0900

    Top画面作成しました。

--- app/src/main/java/com/example/sgapp/MainActivity.kt
+++ app/src/main/java/com/example/sgapp/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import com.example.sgapp.ui.login.LoginActivity
+//import com.example.sgapp.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         var start_button = findViewById<ImageView>(R.id.start_button)
         start_button.setOnClickListener{
-            val intent = Intent(this,LoginActivity::class.java)
-            startActivity(intent)
+//            val intent = Intent(this,LoginActivity::class.java)
+//            startActivity(intent)
         }
     }
 }
