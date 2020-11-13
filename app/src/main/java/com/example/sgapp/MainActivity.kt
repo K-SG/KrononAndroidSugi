3a4416f0898798af574d4308bb335d941bab1d67
K_Sugi <67626325+SugiKoki@users.noreply.github.com>
2020-11-13 15:26:53 +0900

    start画面作成しました

--- app/src/main/java/com/example/sgapp/MainActivity.kt
+++ app/src/main/java/com/example/sgapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.sgapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import com.example.sgapp.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var start_button = findViewById<ImageView>(R.id.start_button)
+        start_button.setOnClickListener{
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
