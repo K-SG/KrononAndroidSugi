09cc54a762158487ffabc65a10ba2ef83c610a31
K_Sugi <67626325+SugiKoki@users.noreply.github.com>
2020-11-19 22:38:42 +0900

    テストで画面遷移ができるように。

--- app/src/main/java/com/example/sgapp/MainActivity.kt
+++ app/src/main/java/com/example/sgapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.sgapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var start_button = findViewById<ImageView>(R.id.start_button)
+        start_button.setOnClickListener {
+            val intent = Intent(this, MainButtomActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
