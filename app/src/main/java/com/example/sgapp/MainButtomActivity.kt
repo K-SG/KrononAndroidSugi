73af814b62982b9cc6f9b2c64d510a5d046f884e
K_Sugi <67626325+SugiKoki@users.noreply.github.com>
2020-11-20 12:04:26 +0900

    ナビゲーションバーを作成しました。

--- app/src/main/java/com/example/sgapp/MainButtomActivity.kt
+++ app/src/main/java/com/example/sgapp/MainButtomActivity.kt
@@ -13,17 +13,18 @@ class MainButtomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_buttom)
+//        supportActionBar!!.hide()
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
