45d8ef1e05b7afd5c42293da943269a85db33fcc
K_Sugi <67626325+SugiKoki@users.noreply.github.com>
2020-12-01 16:29:43 +0900

    groupViewに入れました

--- app/src/main/java/com/example/sgapp/NewUserCreateActivity.kt
+++ app/src/main/java/com/example/sgapp/NewUserCreateActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.app.ActionBar
 
 class NewUserCreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
