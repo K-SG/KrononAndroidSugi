44d80e0ff4affaf5ee0fb329aa0bd5b722333a18
K_Sugi <67626325+SugiKoki@users.noreply.github.com>
2020-12-15 19:49:35 +0900

    予定詳細から飛べるようにしました。

--- app/src/main/java/com/example/sgapp/DetailScheduleActivity.kt
+++ app/src/main/java/com/example/sgapp/DetailScheduleActivity.kt
@@ -1,7 +1,9 @@
 package com.example.sgapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 
 class DetailScheduleActivity : AppCompatActivity() {
@@ -9,8 +11,19 @@ class DetailScheduleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_schedule)
         var backButton = findViewById<ImageView>(R.id.back_button)
+        var editScheduleButton = findViewById<ImageButton>(R.id.update_button)
+        var deleteScheduleButton = findViewById<ImageButton>(R.id.delete_button)
+
         backButton.setOnClickListener{
             finish()
         }
+        editScheduleButton.setOnClickListener{
+            val intent = Intent(this,CreateScheduleActivity::class.java)
+            startActivity(intent)
+        }
+        deleteScheduleButton.setOnClickListener{
+            finish()
+        }
+
     }
 }
