3be67644daee3877df40b155347caa2509c97d05
SugiKoki <67626325+SugiKoki@users.noreply.github.com>
2021-01-31 21:37:17 +0900

    datePickerチャレンジ

--- app/src/main/java/com/example/sgapp/CreateScheduleActivity.kt
+++ app/src/main/java/com/example/sgapp/CreateScheduleActivity.kt
@@ -1,14 +1,17 @@
 package com.example.sgapp
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 
 class CreateScheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_schedule)
+
         val backButton = findViewById<ImageView>(R.id.back_button)
         backButton.setOnClickListener{
             finish()
@@ -17,5 +20,21 @@ class CreateScheduleActivity : AppCompatActivity() {
         okButton.setOnClickListener{
             finish()
         }
+        val dateText = findViewById<TextView>(R.id.date)
+        dateText.setOnClickListener {
+            showDatePicker()
+        }
+    }
+    private fun showDatePicker() {
+        var datepickerText = findViewById<TextView>(R.id.date)
+        val datePickerDialog = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener() {view, year, month, dayOfMonth->
+                datepickerText.text = "選択した日付は「${year}/${month + 1}/${dayOfMonth}」です"
+            },
+            2020,
+            3,
+            1)
+        datePickerDialog.show()
     }
 }
