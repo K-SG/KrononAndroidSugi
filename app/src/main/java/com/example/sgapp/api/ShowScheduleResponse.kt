6657c47bf9f7587b2b3f9ce5d172f588f2c87d98
SugiKoki <67626325+SugiKoki@users.noreply.github.com>
2021-02-09 01:10:03 +0900

    showSchedule一応出来た

--- app/src/main/java/com/example/sgapp/api/ShowScheduleResponse.kt
+++ app/src/main/java/com/example/sgapp/api/ShowScheduleResponse.kt
@@ -2,25 +2,100 @@ package com.example.sgapp.api
 
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
+import java.text.SimpleDateFormat
 
-class ShowScheduleResponse (
+class ScheduleData(
+        @SerializedName("schedule_id")
+        val schedule_id: Int?,
+
+        @SerializedName("title")
+        val title: String?,
+
+        @SerializedName("schedule_date")
+        val schedule_date: String?,
+
+        @SerializedName("place")
+        val place: Int?,
+
+        @SerializedName("start_time")
+        val start_time: String?,
+
+        @SerializedName("end_time")
+        val end_time: String?,
+
+        @SerializedName("content")
+        val content: String?
+)
+
+class ShowScheduleResponse(
         @SerializedName("success")
         val success: Boolean?,
+
         @SerializedName("code")
         val code: Int?,
+
         @SerializedName("data")
-        var data: Array<ShowSchedule>?
-)
+        var data: Array<ShowScheduleData>? = null,
 
-class ShowSchedule(
+        @SerializedName("message")
+        var message: String? = null
+){
+        fun getScheduleShortArray() : Array<Array<ScheduleShortData?>?>?{
+
+                val dataArray = this.data ?: return null
+
+                val scheduleShortArray : Array<Array<ScheduleShortData?>?> = arrayOfNulls(dataArray.size)
+                for(i in dataArray.indices){
+                        val scheduleArray = dataArray[i].schedules
+
+                        if(scheduleArray == null){
+                                scheduleShortArray[i] = arrayOfNulls(0)
+                                break
+                        }
+                        scheduleShortArray[i] = arrayOfNulls(scheduleArray.size)
+                        for(j in scheduleArray.indices){
+
+                                val schedule = scheduleArray[j]
+
+                                val df = SimpleDateFormat("HH:mm:ss")
+                                val startTime = df.parse(schedule.start_time)
+                                val startMinutes = startTime.minutes + startTime.hours*60
+                                val endTime = df.parse(schedule.end_time)
+                                val endMinutes = endTime.minutes + endTime.hours*60
+
+                                scheduleShortArray[i]?.set(j,
+                                        ScheduleShortData(schedule?.schedule_id!!,schedule?.title!!,startMinutes,endMinutes,schedule.place!!)
+                                )
+                        }
+                }
+                return scheduleShortArray
+        }
+
+        fun getNamesArray() : Array<String>?{
+                val dataArray = this.data ?: return null
+                var nameArray : Array<String> = Array(dataArray.size){""}
+
+                for(i in dataArray.indices){
+                        nameArray[i] = (dataArray[i].name.toString())
+                }
+
+                return nameArray
+        }
+}
+
+class ShowScheduleData(
         @SerializedName("name")
-        var name: String? = null,
+        val name: String?,
+
         @SerializedName("schedules")
-        var schedules: Array<String>?
+        var schedules: Array<ScheduleData>? = null
 )
 
-class ScheduleDate(
-        @SerializedName("date")
-        var date: String?
+class ScheduleShortData(
+        val scheduleId: Int,
+        val title: String,
+        val startTime: Int,
+        val endTime: Int,
+        val place: Int
 )
 
--- app/src/main/java/com/example/sgapp/ui/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/example/sgapp/ui/schedule/ScheduleFragment.kt
@@ -14,30 +14,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.retrofit2_kotlin.Retrofit2.KrononService
 import com.example.sgapp.*
 import com.example.sgapp.api.*
 import com.example.sgapp.api.KrononClient.Companion.BaseUrl
-import com.example.sgapp.ui.login.LoginActivity
 import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
 import java.util.*
 
 
 class ScheduleFragment : Fragment() {
 
-    private var dateManager: DateManager? = null
     private lateinit var dashboardViewModel: ScheduleViewModel
     private var root: View? = null
     private var timeFontSize = 10f
-    private var startDayTime = 8
-    private var endDayTime = 20
+    private val startHour = 8
+    private val endHour = 20
     private var mContext :Context? = null
     private var textView :TextView? = null
     private val stepY = 20f
@@ -52,15 +52,12 @@ class ScheduleFragment : Fragment() {
     private var widthPixel = 0
     private var widthContent = 0
     private var scale = 0f
-    var names : Array<String> = arrayOf()
-    private val nx = names.size
+//    var names : Array<String> = arrayOf()
+    private var names = arrayOf("中根", "奥野", "杉")
+    private val nx = 3//names.size
     var calendar: Calendar = Calendar.getInstance()
-    val schedules = arrayOf(
-        ScheduleModel(1, 1, "2020/12/20","タスク１" ,10 * 60, 11 * 60 + 30, 0),
-        ScheduleModel(0, 2, "2020/12/20","タスク１" ,15 * 60, 16 * 60 + 30, 2)
-    )
-//    val schedules = arrayOf<String>()
-
+    private var scheduleArray :
+            Array<Array<ScheduleShortData?>?>?= null
     companion object {
     }
 
@@ -87,8 +84,7 @@ class ScheduleFragment : Fragment() {
         //日付フォーマット
         var dateDisplay: String = DateFormat.format("yyyy年MM月dd日(EEE)の予定", date).toString()
         dateText?.text = dateDisplay
-//        names = arrayOf("中根", "奥野", "杉")
-        getAPI()
+        getAPI(root!!)
 
         newScheduleButton?.setOnClickListener(View.OnClickListener {
             val intent = Intent(activity, CreateScheduleActivity::class.java)
@@ -116,180 +112,39 @@ class ScheduleFragment : Fragment() {
         val blackboardContainer = root?.findViewById<RelativeLayout>(R.id.blackboard)
         blackboardContainer?.removeAllViews()
         //予定ブロックの幅
-        widthContent = ((widthPixel - lineReduceLength * scale) / nx).toInt()
+//        widthContent = ((widthPixel - lineReduceLength * scale) / nx).toInt()
 
-        blackboardContainer?.let { viewName(it) }
+//        blackboardContainer?.let { viewName(it) }
 
-        //線描画
-        blackboardContainer?.let { drawLine(R.drawable.stlike, 0, it) }
+//        //線描画
+//        blackboardContainer?.let { drawLine(R.drawable.stlike, 0, it) }
+//
+//
+//        //点線描画
+//        blackboardContainer?.let { drawLine(R.drawable.dotted, 2, it) }
 
-
-        //点線描画
-        blackboardContainer?.let { drawLine(R.drawable.dotted, 2, it) }
-
-        showTime(blackboardContainer)
+//        showTime(blackboardContainer)
 
         //予定表示
-        blackboardContainer?.let { viewSchedule(it) }
+//        blackboardContainer?.let { viewSchedule(it) }
         return root
     }
-
-    //名前表示
-    @SuppressLint("ResourceAsColor")
-    private fun viewName(layout: RelativeLayout){
-        mContext = activity
-        
-        val nx = names.size
-        for (i in 0 until nx) {
-            val textView = TextView(mContext)
-            textView.text = names[i]
-
-            // テキストサイズ 30sp
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, timeFontSize)
-            textView?.setTextColor(Color.WHITE)
-
-
-            val param = RelativeLayout.LayoutParams(
-                widthContent,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-            )
-
-            param.addRule(RelativeLayout.ALIGN_PARENT_TOP)
-            param.addRule(RelativeLayout.ALIGN_PARENT_LEFT)
-
-
-            param.setMargins(
-                (widthContent * i + nameStartOffsetX * scale).toInt(),
-                (nameStartOffsetY * scale).toInt(),
-                0,
-                0
-            )
-
-            textView.layoutParams = param
-
-            textView.gravity = Gravity.CENTER
-
-            layout.addView(textView)
-        }
-    }
-
-    private fun drawLine(pattern: Int, offset: Int, layout: RelativeLayout){
-        mContext = activity
-        for (i in 0 until (endDayTime - startDayTime)) {
-            val imageView = ImageView(mContext)
-
-            imageView.setImageResource(pattern)
-
-            val param =
-                RelativeLayout.LayoutParams(
-                    (widthPixel - lineReduceLength * scale).toInt(),
-                    (2 * scale).toInt()
-                )
-
-            param.addRule(RelativeLayout.ALIGN_PARENT_TOP)
-            param.addRule(RelativeLayout.ALIGN_PARENT_LEFT)
-
-            param.setMargins(
-                (lineStartOffsetX * scale).toInt(),
-                (stepY * (4 * i + offset) * scale + lineStartOffsetY * scale).toInt(),
-                0,
-                0
-            )
-
-            imageView.layoutParams = param
-
-            layout.addView(imageView)
-        }
-    }
-
-    private fun showTime(blackboardContainer: RelativeLayout?) {
-        mContext = activity
-        for (i in 0 until (endDayTime - startDayTime+1)) {
-            textView = TextView(mContext)
-            textView?.text = "${i + startDayTime}:00"
-            textView?.setTextSize(TypedValue.COMPLEX_UNIT_SP, timeFontSize)
-            textView?.setTextColor(Color.WHITE)
-
-            val param = RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-            )
-            param.addRule(RelativeLayout.ALIGN_PARENT_TOP)
-            param.addRule(RelativeLayout.ALIGN_PARENT_LEFT)
-            param.setMargins(
-                (timeStartOffsetX * scale).toInt(),
-                (stepY * 4 * i * scale + timeStartOffsetY * scale).toInt(),
-                0,
-                0
-            )
-            textView?.layoutParams = param
-
-            blackboardContainer?.addView(textView)
-        }
-    }
-    private fun viewSchedule(layout: RelativeLayout) {
-        mContext = activity
-        for (schedule in schedules) {
-            val contentHeight = (schedule.endTime - schedule.startTime) / 15
-            val contentStart = (schedule.startTime - startDayTime * 60) / 15
-            val contentColor = schedule.place
-
-            val textView = TextView(mContext)
-            textView.text = schedule.title
-
-            // テキストサイズ 30sp
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, timeFontSize)
-            textView.setTextColor(Color.GRAY)
-
-
-            when (contentColor) {
-                0 -> textView.setBackgroundResource(R.color.place_home)
-                1 -> textView.setBackgroundResource(R.color.place_office)
-                2 -> textView.setBackgroundResource(R.color.place_away)
-
-            }
-
-            val param =
-                RelativeLayout.LayoutParams(
-                    (widthContent - contentMargin * 2).toInt(),
-                    (stepY * scale * contentHeight).toInt()
-                )
-
-            param.addRule(RelativeLayout.ALIGN_PARENT_TOP)
-            param.addRule(RelativeLayout.ALIGN_PARENT_LEFT)
-
-
-            param.setMargins(
-                (widthContent * schedule.userId + lineStartOffsetX * scale + contentMargin).toInt(),
-                (stepY * contentStart * scale + lineStartOffsetY * scale).toInt(),
-                contentMargin.toInt(),
-                0
-            )
-            textView.setOnClickListener(View.OnClickListener {
-                var intent = Intent(activity,DetailScheduleActivity::class.java)
-                startActivity(intent)
-            })
-
-            textView.layoutParams = param
-
-            textView.gravity = Gravity.CENTER
-
-            layout.addView(textView)
-
-        }
-    }
-    fun getAPI(){
+    fun getAPI(root: View){
         val pref = activity?.getSharedPreferences("user_data", Context.MODE_PRIVATE)
         var token = pref?.getString("token", "")
         val retrofit = Retrofit.Builder()
             .baseUrl(BaseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-        var date = "2021-01-01"
+        val format = SimpleDateFormat("yyyy-MM-dd")
+        val today = Date()
+        var dateAPI = format.format(today)
+        //Date型のも用意
+        val date = format.parse(dateAPI)
         token = "Bearer $token"
         val service = retrofit.create(KrononService::class.java)
 //        val scheduleDate = ScheduleDate(date)
-        val call = service.showSchedules(date,token,"application/json")
+        val call = service.showSchedules(dateAPI,token,"application/json")
 
         call.enqueue(object : Callback<ShowScheduleResponse> {
             override fun onResponse(
@@ -297,7 +152,11 @@ class ScheduleFragment : Fragment() {
                 response: Response<ShowScheduleResponse>
             ) {
                 if (response.code() == 200) {
-                    names = arrayOf("中根", "奥野", "杉")
+                    var responseBody = response.body()
+                    Log.i("response", responseBody?.data.toString())
+                    names = responseBody?.getNamesArray()!!
+                    scheduleArray = responseBody.getScheduleShortArray()
+                    showSchedules(root,date)
                 } else {
                     val responseError = response.errorBody()
                     val exceptionBody = Gson().fromJson(responseError?.string(), LoginUserErrorResponse::class.java)
@@ -329,5 +188,204 @@ class ScheduleFragment : Fragment() {
 
         })
     }
+
+    private fun showSchedules(root: View, date:Date){
+        val dateTextView = root.findViewById<TextView>(R.id.date)
+        val dateDisplay: String = DateFormat.format("yyyy年MM月dd日(EEE)の予定", date).toString()
+        dateTextView?.text = dateDisplay
+
+        //端末の幅のサイズ[pixel]
+        widthPixel = resources.displayMetrics.widthPixels
+        Log.i("Tag", "width:$widthPixel")
+
+        // dp 設定
+        scale = resources.displayMetrics.density
+
+        //予定ブロックの幅
+        widthContent = ((widthPixel - lineReduceLength * scale) / nx).toInt()
+
+        //配置するレイアウトの取得
+        val blackboardContainer = root.findViewById<RelativeLayout>(R.id.blackboard)
+
+
+        //名前表示
+        viewName(blackboardContainer)
+
+        //線描画
+        drawLine(R.drawable.stlike, 0, endHour - startHour + 1, blackboardContainer)
+
+
+        //点線描画
+        drawLine(R.drawable.dotted, 2, endHour - startHour + 1, blackboardContainer)
+
+
+        //時間表示
+        viewTime(blackboardContainer)
+
+        //予定表示
+        viewSchedule(blackboardContainer)
+    }
+
+    //名前表示
+    private fun viewName(layout: RelativeLayout){
+        for (i in 0 until nx) {
+            val textView = TextView(activity)
+            textView.text = names[i]
+
+            // テキストサイズ 30sp
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, timeFontSize)
+
+
+            val param = RelativeLayout.LayoutParams(
+                widthContent,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+            )
+
+            param.addRule(RelativeLayout.ALIGN_PARENT_TOP)
+            param.addRule(RelativeLayout.ALIGN_PARENT_LEFT)
+
+
+            param.setMargins(
+                (widthContent * i + nameStartOffsetX * scale).toInt(),
+                (nameStartOffsetY * scale).toInt(),
+                0,
+                0
+            )
+
+            textView.layoutParams = param
+
+            textView.gravity = Gravity.CENTER
+
+            layout.addView(textView)
+        }
+    }
+
+    private fun drawLine(pattern: Int, offset: Int, number : Int,layout: RelativeLayout){
+        for (i in 0 until (number)) {
+            val imageView = ImageView(activity)
+
+            imageView.setImageResource(pattern)
+
+            val param =
+                RelativeLayout.LayoutParams(
+                    (widthPixel - lineReduceLength * scale).toInt(),
+                    (2 * scale).toInt()
+                )
+
+            param.addRule(RelativeLayout.ALIGN_PARENT_TOP)
+            param.addRule(RelativeLayout.ALIGN_PARENT_LEFT)
+
+
+
+            param.setMargins(
+                (lineStartOffsetX * scale).toInt(),
+                (stepY * (4 * i + offset) * scale + lineStartOffsetY * scale).toInt(),
+                0,
+                0
+            )
+
+            imageView.layoutParams = param
+
+            layout.addView(imageView)
+        }
+    }
+
+    private fun viewSchedule(layout: RelativeLayout) {
+        for ( i in  scheduleArray!!.indices) {
+
+            val schedules = scheduleArray!![i]
+
+            for(schedule in schedules!!) {
+
+                val contentHeight = (schedule!!.endTime - schedule!!.startTime) / 15
+                val contentStart = (schedule!!.startTime - startHour * 60) / 15
+                val contentColor = schedule!!.place
+
+                val textView = TextView(activity)
+                textView.text = schedule.title
+
+                // テキストサイズ 30sp
+                textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, timeFontSize)
+                activity?.let { ContextCompat.getColor(it, R.color.common_text) }?.let {
+                    textView.setTextColor(
+                        it
+                    )
+                }
+
+
+                when (contentColor) {
+                    0 -> textView.setBackgroundResource(R.color.place_home)
+                    1 -> textView.setBackgroundResource(R.color.place_office)
+                    2 -> textView.setBackgroundResource(R.color.place_away)
+
+                }
+
+                val param =
+                    RelativeLayout.LayoutParams(
+                        (widthContent - contentMargin * 2).toInt(),
+                        (stepY * scale * contentHeight).toInt()
+                    )
+
+                param.addRule(RelativeLayout.ALIGN_PARENT_TOP)
+                param.addRule(RelativeLayout.ALIGN_PARENT_LEFT)
+
+
+                param.setMargins(
+                    (widthContent * i + lineStartOffsetX * scale + contentMargin).toInt(),
+                    (stepY * contentStart * scale + lineStartOffsetY * scale).toInt(),
+                    contentMargin.toInt(),
+                    0
+                )
+
+                textView.layoutParams = param
+
+                textView.gravity = Gravity.CENTER
+
+                layout.addView(textView)
+
+                textView.setOnClickListener(View.OnClickListener {
+                    var intent = Intent(activity, DetailScheduleActivity::class.java)
+                    startActivity(intent)
+                })
+            }
+        }
+    }
+    //時間表示
+    private fun viewTime(layout: RelativeLayout){
+
+        for (i in 0 until (endHour - startHour + 1)) {
+            val textView = TextView(activity)
+            textView.text = "${i + startHour}:00"
+
+            // テキストサイズ 30sp
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, timeFontSize)
+
+
+            val param =
+                RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+                )
+
+            param.addRule(RelativeLayout.ALIGN_PARENT_TOP)
+            param.addRule(RelativeLayout.ALIGN_PARENT_LEFT)
+
+
+            param.setMargins(
+                (timeStartOffsetX * scale).toInt(),
+                (stepY * 4 * i * scale + timeStartOffsetY * scale).toInt(),
+                0,
+                0
+            )
+
+            textView.layoutParams = param
+
+            layout.addView(textView)
+
+
+        }
+    }
+
+
 
 }
